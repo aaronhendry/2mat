c2be342a2e7c0537af804636d2d7bc104ba01545
Aaron Hendry <aaronhendry@users.noreply.github.com>
2021-03-19 17:50:36 +1300

    Added util file for useful functions

---
+++ src/util.cpp
@@ -0,0 +1,31 @@
+/*
+ * 2mat/util.hpp -- implementation of util functions
+ * 
+ * Version: 1.0
+ * Date created: 2021 March 12
+ * Copyright (c) 2021 Aaron Hendry
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "util.hpp"
+
+namespace mat
+{
+
+    mfile_error::mfile_error(const std::string &m)
+    :
+        runtime_error(m)
+    {}
+
+    mfile_error::~mfile_error() throw(){}
+}
---
+++ src/util.hpp
@@ -0,0 +1,60 @@
+/*
+ * 2mat/util.hpp -- useful functions for converting to MATLAB format
+ * 
+ * Version: 1.0
+ * Date created: 2021 March 12
+ * Copyright (c) 2021 Aaron Hendry
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef TOO_MAT_UTIL_H
+#define TOO_MAT_UTIL_H
+
+#include <stdexcept>
+
+namespace mat
+{
+
+    /*
+     * uint mat::ceil8(uint)
+     * 
+     * Rounds an unsigned integer up to the nearest 8
+     * 
+     * EXAMPLE:
+     * 
+     * uint n = ceil8(15); // Returns 16u
+     * 
+     * INPUT:
+     *  n   (uint)  the unsigned integer to round up
+     * RETURNS:
+     *  the integer n rounded up to the nearest 8
+     */
+    inline unsigned int ceil8 (unsigned int n)
+    {
+        return (n+7)&~7;
+    }
+
+    /*
+     * mat::mfile_error
+     * 
+     * Error class for exceptions that occur during creating MATLAB .mat files
+     */
+    class mfile_error : public std::runtime_error
+    {
+    public:
+        mfile_error(const std::string &m);
+        ~mfile_error() throw();
+    };
+}
+
+#endif
