2df6f9701acc12363b38daeedb8207147f9ba88a
Aaron Hendry <aaronhendry@users.noreply.github.com>
2021-03-19 17:45:40 +1300

    Added fwriter class
    This is used to write binary data to the .mat files, with the
    option of adding a compressing filter to the writer.

---
+++ src/io/fwriter.cpp
@@ -0,0 +1,152 @@
+/*
+ * 2mat/io/fwriter.hpp -- Writes binary data to a file
+ * 
+ * Version: 1.0
+ * Date created: 2021 March 18
+ * Copyright (c) 2021 Aaron Hendry
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "fwriter.hpp"
+#include <cstring>
+
+namespace mat
+{
+    filter::filter(FILE *file)
+    :
+        fptr(file)
+    {}
+
+
+    nofilter::nofilter(FILE *file)
+    :
+        filter(file)
+    {}
+
+    dim_t nofilter::write(const unsigned char *data, dim_t bytes)
+    {
+        return fwrite(data,1,bytes,fptr);
+    }
+
+    void nofilter::flush(){}
+
+    zfilter::zfilter(FILE *file, unsigned int level)
+    :
+        filter(file),
+        bptr(buffer),
+        bend(buffer+MAT_ZCHUNK),
+        level(level)
+    {
+
+        strm.zalloc = Z_NULL;
+        strm.zfree = Z_NULL;
+        strm.opaque = Z_NULL;
+        auto ret = deflateInit(&strm, level);
+        if (ret != Z_OK)
+            throw mfile_error("Could not initiakuse zlib library");
+    }
+
+    zfilter::~zfilter()
+    {
+        flush();
+        deflateEnd(&strm);
+    }
+
+    void zfilter::compress(bool finish)
+    {
+        strm.avail_in = bptr-buffer;
+        strm.next_in = buffer;
+        strm.avail_out = MAT_ZCHUNK;
+        strm.next_out = zbuffer;
+        auto f = finish ? Z_FINISH : Z_NO_FLUSH;
+        do {
+            auto ret = deflate(&strm,f);
+            if (ret == Z_STREAM_ERROR) throw mfile_error("Could not compress data element");
+            auto have = MAT_ZCHUNK-strm.avail_out;
+            fwrite(zbuffer,1,have,fptr);
+        } while (strm.avail_out == 0);
+        bptr = buffer;
+    }
+
+    dim_t zfilter::write(const unsigned char *data, dim_t bytes)
+    {
+        dim_t avail = bend-bptr;
+        if (avail > bytes)
+        {
+            memcpy(bptr,data,bytes);
+            bptr += bytes;
+            return bytes;
+        }
+        dim_t off = 0;
+        do
+        {
+            memcpy(bptr,data+off,avail);
+            off += avail;
+            compress(false);
+        } while(bytes-off > avail);
+        memcpy(bptr,data+off,bytes-off);
+        return bytes;
+    }
+
+    void zfilter::flush()
+    {
+        compress(true);
+    }
+
+    fwriter::fwriter(const std::string &path)
+    :
+        fptr(fopen(path.c_str(),"wb"))
+    {
+        if (!fptr) throw mfile_error("Could not open file");
+        filt = new nofilter(fptr);
+    }
+
+    fwriter::~fwriter()
+    {
+        close();
+    }
+
+    void fwriter::rmfilter()
+    {
+        if (filt) delete filt;
+        filt = new nofilter(fptr);
+    }
+
+    dim_t fwriter::tellp() const
+    {
+        if (!fptr) throw mfile_error("Cannot tell closed file");
+        return ftell(fptr);
+    }
+    dim_t fwriter::seekp(dim_t pos, ios::filepos whence)
+    {
+        if (!fptr) throw mfile_error("Cannot seek closed file");
+        filt->flush();
+        return fseek(fptr,pos,whence);
+    }
+
+    dim_t fwriter::write(const std::string &str)
+    {
+        if (!fptr) throw mfile_error("Cannot write to closed file");
+        return filt->write((const unsigned char *)&str[0],str.size());
+    }
+
+    void fwriter::close()
+    {
+        if (filt) delete filt;
+        filt = NULL;
+        if (!fptr) return;
+        fclose(fptr);
+        fptr = NULL;
+    }
+
+}
---
+++ src/io/fwriter.hpp
@@ -0,0 +1,180 @@
+/*
+ * 2mat/io/fwriter.hpp -- Writes binary data to a file
+ * 
+ * Version: 1.0
+ * Date created: 2021 March 18
+ * Copyright (c) 2021 Aaron Hendry
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef TOO_MAT_IO_FWRITER_H
+#define TOO_MAT_IO_FWRITER_H
+
+#include "../types.hpp"
+#include "../util.hpp"
+
+#include <cstdio>
+#include <string>
+#include <zlib.h>
+#include <type_traits>
+#include <memory>
+
+#ifndef MAT_ZCHUNK
+#define MAT_ZCHUNK 1048576
+#endif
+
+#ifndef MAT_ZLEVEL
+#define MAT_ZLEVEL 8
+#endif
+
+#ifndef MAT_FBUF
+#define MAT_FBUF 4096
+#endif
+
+namespace mat
+{
+
+    namespace ios
+    {  
+        enum filepos
+        {
+            beg=SEEK_SET,
+            cur=SEEK_CUR,
+            end=SEEK_END
+        };
+    }
+
+    class filter
+    {
+    protected:
+        FILE *fptr;
+    public:
+        filter(FILE *file);
+        virtual ~filter() = default;
+
+        virtual dim_t write(const unsigned char *data, dim_t bytes) = 0;
+        virtual void flush() = 0;
+    };
+
+    class nofilter : public filter
+    {
+    public:
+        nofilter(FILE *file);
+        virtual ~nofilter() = default;
+
+        virtual dim_t write(const unsigned char *data, dim_t bytes);
+        virtual void flush();
+    };
+
+    class zfilter : public filter
+    {
+        unsigned char *bptr, *bend;
+        unsigned int level;
+        unsigned char buffer[MAT_ZCHUNK];
+        unsigned char zbuffer[MAT_ZCHUNK];
+
+        z_stream strm;
+
+        void compress(bool finish);
+    public:
+        zfilter(FILE *file, unsigned int level = MAT_ZLEVEL);
+        ~zfilter();
+
+        virtual dim_t write(const unsigned char *data, dim_t bytes);
+        virtual void flush();
+    };
+
+    class fwriter
+    {
+        FILE *fptr;
+        filter *filt;
+    public:
+        fwriter(const std::string &path);
+        ~fwriter();
+
+        template <typename T>
+        void addfilter();
+        void rmfilter();
+
+        dim_t tellp() const;
+        dim_t seekp(dim_t pos, ios::filepos = ios::beg);
+
+        template <typename T, typename U=T>
+        dim_t write(T val);
+        template <typename T, typename U=T>
+        dim_t write_n(T val, dim_t n);
+        template <typename T, typename U=T>
+        dim_t write(T *ptr, dim_t n);
+        dim_t write(const std::string &str);
+
+        void close();
+
+    };
+
+    template <typename T>
+    void fwriter::addfilter()
+    {
+        if (filt) delete filt;
+        filt = new T(fptr);
+    }
+
+    template <typename T, typename U>
+    dim_t fwriter::write(T val)
+    {
+        if (!fptr) throw mfile_error("Cannot write to closed file");
+        if (std::is_same<T,U>::value)
+        {
+            filt->write((unsigned char *)&val,sizeof(T));
+            return sizeof(T);
+        }
+        U uval = (U)val;
+        filt->write((unsigned char *)&uval,sizeof(U));
+        return sizeof(U);
+    }
+
+    template <typename T, typename U>
+    dim_t fwriter::write_n(T val, dim_t n)
+    {
+        if (!fptr) throw mfile_error("Cannot write to closed file");
+        if (std::is_same<T,U>::value)
+        {
+            for (uint i = 0; i < n; ++i)
+                filt->write((unsigned char *)&val,sizeof(T));
+            return n*sizeof(T);
+        }
+        U uval = (U)val;
+        for (uint i = 0; i < n; ++i)
+            filt->write((unsigned char *)&uval,sizeof(U));
+        return n*sizeof(U);
+    }
+
+    template <typename T, typename U>
+    dim_t fwriter::write(T *ptr, dim_t n)
+    {
+        if (!fptr) throw mfile_error("Cannot write to closed file");
+        if (std::is_same<T,U>::value)
+        {
+            return filt->write((unsigned char *)ptr,n*sizeof(T));
+        }
+        U val;
+        for (uint i = 0; i < n; ++i)
+        {
+            val = (U)ptr[i];
+            filt->write((unsigned char *)&val,sizeof(U));
+        }
+        return n*sizeof(U);
+    }
+
+}
+
+#endif
