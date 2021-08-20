19f796d82774c22756282cf6bdb9caa03db597e2
Aaron Hendry <aaronhendry@users.noreply.github.com>
2021-08-20 16:21:13 +1200

    Added utility file for includes

---
+++ inc/2mat.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Aaron on 20/08/2021.
+//
+
+#ifndef INC_2MAT_2MAT_HPP
+#define INC_2MAT_2MAT_HPP
+
+// UTIL import file
+
+#include "datenum.hpp"
+#include "element.hpp"
+#include "file.hpp"
+#include "matrix.hpp"
+#include "mstruct.hpp"
+
+#endif //INC_2MAT_2MAT_HPP
